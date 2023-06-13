f45b6aea7784205e3a2742e7aa56e48aab9ea303
David Neukirchen <david.neukirchen@gmail.com>
2023-06-13 13:59:04 +0200

    Update version

--- src/app/mat-select-search/ngx-mat-select-search.module.ts
+++ src/app/mat-select-search/ngx-mat-select-search.module.ts
@@ -5,20 +5,20 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import { NgModule } from '@angular/core';
-import { MatSelectSearchComponent } from './mat-select-search.component';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { MatSelectSearchComponent } from "./mat-select-search.component";
+import { MatLegacyButtonModule as MatButtonModule } from "@angular/material/legacy-button";
+import { MatLegacyCheckboxModule as MatCheckboxModule } from "@angular/material/legacy-checkbox";
+import { MatIconModule } from "@angular/material/icon";
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from "@angular/material/legacy-progress-spinner";
+import { MatLegacyTooltipModule as MatTooltipModule } from "@angular/material/legacy-tooltip";
+import { CommonModule } from "@angular/common";
 
-import { MatSelectSearchClearDirective } from './mat-select-search-clear.directive';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatSelectNoEntriesFoundDirective } from './mat-select-no-entries-found.directive';
+import { MatSelectSearchClearDirective } from "./mat-select-search-clear.directive";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatSelectNoEntriesFoundDirective } from "./mat-select-no-entries-found.directive";
 
-export const MatSelectSearchVersion = '6.0.0';
+export const MatSelectSearchVersion = "16.0.0";
 export { MatSelectSearchClearDirective };
 export { MatSelectNoEntriesFoundDirective };
 
@@ -30,18 +30,17 @@ export { MatSelectNoEntriesFoundDirective };
     MatCheckboxModule,
     MatIconModule,
     MatProgressSpinnerModule,
-    MatTooltipModule
+    MatTooltipModule,
   ],
   declarations: [
     MatSelectSearchComponent,
     MatSelectSearchClearDirective,
-    MatSelectNoEntriesFoundDirective
+    MatSelectNoEntriesFoundDirective,
   ],
   exports: [
     MatSelectSearchComponent,
     MatSelectSearchClearDirective,
-    MatSelectNoEntriesFoundDirective
-  ]
+    MatSelectNoEntriesFoundDirective,
+  ],
 })
-export class NgxMatSelectSearchModule {
-}
+export class NgxMatSelectSearchModule {}
